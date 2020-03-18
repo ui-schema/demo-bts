22635ccd4b5090a414414f794ec0c43ac8cf1dfd
carolinholat <carolin.holat@gmail.com>
2020-03-18 14:10:49 +0100

    fixed external urls (a instead of react-router-dom)

--- src/component/NavProject.js
+++ src/component/NavProject.js
@@ -1,32 +1,34 @@
 import React from "react";
 import GithubLogo from "./GithubLogo";
-import {Link} from "react-router-dom";
 
 export default () => <React.Fragment>
     <p>
         <span role={'img'} aria-label={'Quick Evaluate CodeSandbox'}>🚀</span>{' '}
-        <Link to={'https://codesandbox.io/s/github/ui-schema/bts-demo/tree/master/?autoresize=1&fontsize=12&hidenavigation=1&module=%2Fsrc%2FSchema%2FDemoEditor.js'}>Run on CodeSandbox</Link>
+        <a target="_blank" href={'https://codesandbox.io/s/github/ui-schema/bts-demo'}>Run on CodeSandbox</a>
     </p>
     <hr style={{opacity: 0.1, margin: '4px 0 4px 26px'}}/>
     <p>
         <span role={'img'} aria-label={'Quick Evaluate Repl.it'}>🚀</span>{' '}
-        <Link to={'https://repl.it/@elbakerino/ui-schema-cra'}>Repl.it Run</Link>&nbsp;|&nbsp;
-        <Link to={'https://repl.it/github/ui-schema/demo-cra'}>Repl.it Clone</Link>
+        <a target="_blank" href={'https://repl.it/@elbakerino/ui-schema-cra'}>Repl.it Run</a>&nbsp;|&nbsp;
+        <a target="_blank" href={'https://repl.it/github/ui-schema/demo-cra'}>Repl.it Clone</a>
     </p>
     <hr style={{opacity: 0.1, margin: '4px 0 4px 26px'}}/>
     <p>
-        <GithubLogo/> <Link to={'https://github.com/ui-schema/demo-cra'}>Source of this demo</Link>
+        <GithubLogo/>
+        <a target="_blank" href={'https://github.com/ui-schema/demo-bts'}>Source of this demo</a>
     </p>
     <hr style={{opacity: 0.2}}/>
     <p>
-        <span role={'img'} aria-label={'Examples'}>📖</span> <Link to={'https://ui-schema.bemit.codes/examples'}>Live-Editor + Examples</Link>
+        <span role={'img'} aria-label={'Examples'}>📖</span> <a target="_blank" href={'https://ui-schema.bemit.codes/examples'}>Live-Editor + Examples</a>
     </p>
     <hr style={{opacity: 0.1, margin: '4px 0 4px 26px'}}/>
     <p>
-        <span role={'img'} aria-label={'Documentation'}>📖</span> <Link to={'https://github.com/ui-schema/ui-schema/blob/master/README.md'}>Documentation, Current Status</Link>
+        <span role={'img'} aria-label={'Documentation'}>📖</span>
+        <a target="_blank" href={'https://github.com/ui-schema/ui-schema/blob/master/README.md'}>Documentation, Current Status</a>
     </p>
     <hr style={{opacity: 0.1, margin: '4px 0 4px 26px'}}/>
     <p>
-        <GithubLogo/> <Link to={'https://github.com/ui-schema/ui-schema'}>Project, Issues</Link>
+        <GithubLogo/>
+        <a target="_blank" href={'https://github.com/ui-schema/ui-schema'}>Project, Issues</a>
     </p>
 </React.Fragment>;
