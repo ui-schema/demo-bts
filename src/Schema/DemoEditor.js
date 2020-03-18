0a107a0981d56b5ea72ffe77b248ff5c3410291b
carolinholat <carolin.holat@gmail.com>
2020-03-18 17:05:39 +0100

    false loading icon corrected

--- src/Schema/DemoEditor.js
+++ src/Schema/DemoEditor.js
@@ -99,8 +99,9 @@ const Editor = () => {
     }, [setStore, setSchema]);
 
     if(!store || !schema) return <div style={{textAlign: 'center', margin: '75px 0'}}>
-        <svg className={["bi", "bi-arrow-repeat"]} width="32" height="32" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clipRule="evenodd"/>
+        <svg className={["bi", "bi-arrow-clockwise"].join(' ')} width="32" height="32" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M10 4.5a5.5 5.5 0 105.5 5.5.5.5 0 011 0 6.5 6.5 0 11-3.25-5.63l-.5.865A5.472 5.472 0 0010 4.5z" clipRule="evenodd"/>
+            <path fillRule="evenodd" d="M10.646 1.646a.5.5 0 01.708 0l2.5 2.5a.5.5 0 010 .708l-2.5 2.5a.5.5 0 01-.708-.708L12.793 4.5l-2.147-2.146a.5.5 0 010-.708z" clipRule="evenodd"/>
         </svg>
 
         <p>Loading Schema</p>
